a57b57d50bf7b28f5359ef9f9af1a6b944587ea0
ZinoChan <devzinochan@gmail.com>
2024-01-06 22:27:58 +0100

    styles: add primary colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,19 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import { withUt } from "uploadthing/tw";
 
-export default {
+export default withUt({
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        "primary-500": "#13B546",
+        "light-100": "#F4F6F6",
+      },
     },
   },
   plugins: [],
-} satisfies Config;
+}) satisfies Config;
